417131e9072a5f596063f0c2f4191f4cdeafa45c
jmjang88 <jmjang88@naver.com>
2026-06-21 21:04:58 +0900

    Revert "[feat] 보너스4 - 데이터 품질/이상치 탐지"
    This reverts commit ab7717241c5a238ffcb375d1b5569bc106d0e7ff.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,6 @@
 from app.models.classification import Classification
-from app.models.data_quality_issue import DataQualityIssue
-from app.models.etl_run import EtlRun
 from app.models.price import StdMarketPrice
 from app.models.publication import Publication
 from app.models.unit_alias import UnitAlias
 
-__all__ = ["Classification", "StdMarketPrice", "Publication", "UnitAlias", "EtlRun", "DataQualityIssue"]
+__all__ = ["Classification", "StdMarketPrice", "Publication", "UnitAlias"]
